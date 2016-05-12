b7a9766f946768f1c1e733e87e01af36e32e32ec
KateSTento <bktento@skymesh.com.au>
2016-05-12 01:49:53 +0000

    fixed problem with application.rb and increased logo font size

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,11 @@ module Nomster
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    #Add the fonts path
+    # Add the fonts path
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
-    #Precompile additional assets
-    config.assets.precompile += %w ( .svg .eot .woff .ttf )
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+
   end
 end

80813587b3a0734718f67536f083a0f1b0553d5d
KateSTento <bktento@skymesh.com.au>
2016-05-14 00:10:16 +0000

    added validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocesable_entity
+    end
   end
 
   def show
@@ -40,7 +44,7 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
-    
+
     @place.destroy
     redirect_to root_path
   end
